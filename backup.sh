8fe35e4aa0c7d95b9f7b31e0459fb4418e8542ef
Jonathan Gazeley <me@jonathangazeley.com>
2023-03-21 17:27:12 +0000

    Fix indent

--- backup.sh
+++ backup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #
-# mysql backup script
-#
+# database backup script
 
 #DBDUMP_DEBUG
 #DBDUMP_TYPE
@@ -19,52 +18,52 @@ fi
 
 if $DBDUMP_HOST ; then
 
-BACKUP_DIR="/backup"
-TIMESTAMP="$(date +%Y%m%d%H%M%S)"
+  BACKUP_DIR="/backup"
+  TIMESTAMP="$(date +%Y%m%d%H%M%S)"
 
-echo "test mysql connection"
-if [ -z "$(mysql -h ${DBDUMP_HOST} -P ${DBDUMP_PORT} -u ${DBDUMP_USER} -B -N -e 'SHOW DATABASES;')" ]; then
-  echo "mysql connection failed! exiting..."
-  exit 1
-fi
+  echo "test mysql connection"
+  if [ -z "$(mysql -h ${DBDUMP_HOST} -P ${DBDUMP_PORT} -u ${DBDUMP_USER} -B -N -e 'SHOW DATABASES;')" ]; then
+    echo "mysql connection failed! exiting..."
+    exit 1
+  fi
 
-echo "started" > ${BACKUP_DIR}/${TIMESTAMP}.state
+  echo "started" > ${BACKUP_DIR}/${TIMESTAMP}.state
 
-echo "delete old backups"
-find ${BACKUP_DIR} -maxdepth 2 -mtime +${KEEP_DAYS} -regex "^${BACKUP_DIR}/.*[0-9]*_.*\.sql\.gz$" -type f -exec rm {} \;
+  echo "delete old backups"
+  find ${BACKUP_DIR} -maxdepth 2 -mtime +${KEEP_DAYS} -regex "^${BACKUP_DIR}/.*[0-9]*_.*\.sql\.gz$" -type f -exec rm {} \;
 
-if $DBDUMP_DB && [[ "$DBDUMP_ALL_DATABASES" != "true" ]] ; then
-  echo "Backing up single db ${DBDUMP_DB}"
-  mkdir -p "${BACKUP_DIR}"/"${DBDUMP_DB}"
-  mysqldump ${DBDUMP_OPTS} -h ${DBDUMP_HOST} -P ${DBDUMP_PORT} -u ${DBDUMP_USER} --databases ${DBDUMP_DB} | gzip > ${BACKUP_DIR}/${DBDUMP_DB}/${TIMESTAMP}_${DBDUMP_DB}.sql.gz
-  rc=$?
-
-elif [ "$DBDUMP_ALL_DATABASES" = "true" ]
-
-  for DBDUMP_DB in $(mysql -h "${DBDUMP_HOST}" -P ${DBDUMP_PORT} -u ${DBDUMP_USER} -B -N -e "SHOW DATABASES;"|egrep -v '^(information|performance)_schema$'); do
-    echo "Backing up db ${DBDUMP_DB}"
+  if $DBDUMP_DB && [[ "$DBDUMP_ALL_DATABASES" != "true" ]] ; then
+    echo "Backing up single db ${DBDUMP_DB}"
     mkdir -p "${BACKUP_DIR}"/"${DBDUMP_DB}"
     mysqldump ${DBDUMP_OPTS} -h ${DBDUMP_HOST} -P ${DBDUMP_PORT} -u ${DBDUMP_USER} --databases ${DBDUMP_DB} | gzip > ${BACKUP_DIR}/${DBDUMP_DB}/${TIMESTAMP}_${DBDUMP_DB}.sql.gz
     rc=$?
-  done
-fi
 
-if [ "$DBDUMP_DEBUG" = true ] ; then
-  echo Contents of ${BACKUP_DIR}
-  ls -lahR ${BACKUP_DIR}
-fi
+  elif [ "$DBDUMP_ALL_DATABASES" = "true" ]
 
-if [ "$rc" != "0" ]; then
-  echo "backup failed"
-  exit 1
-fi
+    for DBDUMP_DB in $(mysql -h "${DBDUMP_HOST}" -P ${DBDUMP_PORT} -u ${DBDUMP_USER} -B -N -e "SHOW DATABASES;"|egrep -v '^(information|performance)_schema$'); do
+      echo "Backing up db ${DBDUMP_DB}"
+      mkdir -p "${BACKUP_DIR}"/"${DBDUMP_DB}"
+      mysqldump ${DBDUMP_OPTS} -h ${DBDUMP_HOST} -P ${DBDUMP_PORT} -u ${DBDUMP_USER} --databases ${DBDUMP_DB} | gzip > ${BACKUP_DIR}/${DBDUMP_DB}/${TIMESTAMP}_${DBDUMP_DB}.sql.gz
+      rc=$?
+    done
+  fi
 
-echo "complete" > ${BACKUP_DIR}/${TIMESTAMP}.state
+  if [ "$DBDUMP_DEBUG" = true ] ; then
+    echo Contents of ${BACKUP_DIR}
+    ls -lahR ${BACKUP_DIR}
+  fi
 
-echo "Disk usage in ${BACKUP_DIR}"
-du -h -d 2 ${BACKUP_DIR}
+  if [ "$rc" != "0" ]; then
+    echo "backup failed"
+    exit 1
+  fi
 
-echo "Backup successful! :-)"
+  echo "complete" > ${BACKUP_DIR}/${TIMESTAMP}.state
+
+  echo "Disk usage in ${BACKUP_DIR}"
+  du -h -d 2 ${BACKUP_DIR}
+
+  echo "Backup successful! :-)"
 else
-  echo "no mysql.host set in values file... nothing to do... exiting :)"
+  echo "no DB host set in values file... nothing to do... exiting :)"
 fi
