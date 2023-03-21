55bcab598c02ec1b5b75eefe218ccda6c4ad6b63
Jonathan Gazeley <me@jonathangazeley.com>
2023-03-21 16:55:31 +0000

    Replace go template with bash conditionals

--- backup.sh
+++ backup.sh
@@ -2,11 +2,22 @@
 #
 # mysql backup script
 #
-{{ if .Values.debug }}
-set -ex
-{{ end }}
 
-{{ if .Values.mysql.host }}
+#DBDUMP_DEBUG
+#DBDUMP_TYPE
+#DBDUMP_HOST
+#DBDUMP_DB
+#DBDUMP_ALL_DB
+#DBDUMP_PORT
+#DBDUMP_USER
+#DBDUMP_PASSWORD
+#DBDUMP_OPTS
+
+if [ "$DBDUMP_DEBUG" = true ] ; then
+  set -ex
+fi
+
+if $DBDUMP_HOST ; then
 
 BACKUP_DIR="/backup"
 TIMESTAMP="$(date +%Y%m%d%H%M%S)"
@@ -19,55 +30,34 @@ fi
 
 echo "started" > ${BACKUP_DIR}/${TIMESTAMP}.state
 
-{{ if or (.Values.persistence.enabled) (.Values.persistentVolumeClaim) }}
-{{ if .Values.housekeeping.enabled }}
 echo "delete old backups"
 find ${BACKUP_DIR} -maxdepth 2 -mtime +${KEEP_DAYS} -regex "^${BACKUP_DIR}/.*[0-9]*_.*\.sql\.gz$" -type f -exec rm {} \;
-{{ end -}}
-{{ end -}}
 
-{{ if and (.Values.mysql.db) (eq .Values.allDatabases.enabled false) }}
-MYSQL_DB="{{ .Values.mysql.db }}"
-echo "Backing up single db ${MYSQL_DB}"
-{{ if .Values.saveToDirectory }}mkdir -p "${BACKUP_DIR}"/"${MYSQL_DB}"{{ end }}
-mysqldump ${MYSQL_OPTS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USERNAME} --databases ${MYSQL_DB} | gzip > ${BACKUP_DIR}/{{ if .Values.saveToDirectory }}${MYSQL_DB}/{{ end }}${TIMESTAMP}_${MYSQL_DB}.sql.gz
-rc=$?
-{{ else if and (.Values.allDatabases.enabled) (eq .Values.allDatabases.singleBackupFile false)}}
-for MYSQL_DB in $(mysql -h "${MYSQL_HOST}" -P ${MYSQL_PORT} -u ${MYSQL_USERNAME} -B -N -e "SHOW DATABASES;"|egrep -v '^(information|performance)_schema$'); do
-  echo "Backing up db ${MYSQL_DB}"
-  {{ if .Values.saveToDirectory }}mkdir -p "${BACKUP_DIR}"/"${MYSQL_DB}"{{ end }}
-  mysqldump ${MYSQL_OPTS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USERNAME} --databases ${MYSQL_DB} | gzip > ${BACKUP_DIR}/{{ if .Values.saveToDirectory }}${MYSQL_DB}/{{ end }}${TIMESTAMP}_${MYSQL_DB}.sql.gz
+if $DBDUMP_DB && [[ "$DBDUMP_ALL_DATABASES" != "true" ]] ; then
+  echo "Backing up single db ${MYSQL_DB}"
+  mkdir -p "${BACKUP_DIR}"/"${MYSQL_DB}"
+  mysqldump ${MYSQL_OPTS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USERNAME} --databases ${MYSQL_DB} | gzip > ${BACKUP_DIR}/${MYSQL_DB}/${TIMESTAMP}_${MYSQL_DB}.sql.gz
   rc=$?
-done
 
-{{ else if and (.Values.allDatabases.enabled) (.Values.allDatabases.singleBackupFile) }}
-echo "Backing up all databases"
-MYSQL_DB="alldatabases"
-{{ if .Values.saveToDirectory }}mkdir -p "${BACKUP_DIR}"/"${MYSQL_DB}"{{ end }}
-mysqldump ${MYSQL_OPTS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USERNAME} --all-databases | gzip > ${BACKUP_DIR}/{{ if .Values.saveToDirectory }}${MYSQL_DB}/{{ end }}${TIMESTAMP}_${MYSQL_DB}.sql.gz
-rc=$?
-{{- end -}}
+elif [ "$DBDUMP_ALL_DATABASES" = "true" ]
 
-{{ if .Values.dumpAllToStdout }}
-mysqldump ${MYSQL_OPTS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USERNAME} --all-databases
-rc=$?
-{{ end }}
+  for MYSQL_DB in $(mysql -h "${MYSQL_HOST}" -P ${MYSQL_PORT} -u ${MYSQL_USERNAME} -B -N -e "SHOW DATABASES;"|egrep -v '^(information|performance)_schema$'); do
+    echo "Backing up db ${MYSQL_DB}"
+    mkdir -p "${BACKUP_DIR}"/"${MYSQL_DB}"
+    mysqldump ${MYSQL_OPTS} -h ${MYSQL_HOST} -P ${MYSQL_PORT} -u ${MYSQL_USERNAME} --databases ${MYSQL_DB} | gzip > ${BACKUP_DIR}/${MYSQL_DB}/${TIMESTAMP}_${MYSQL_DB}.sql.gz
+    rc=$?
+  done
+fi
 
-{{ if .Values.debug }}
+if [ "$DBDUMP_DEBUG" = true ] ; then
   echo Contents of ${BACKUP_DIR}
   ls -lahR ${BACKUP_DIR}
-{{ end }}
+fi
 
 if [ "$rc" != "0" ]; then
   echo "backup failed"
   exit 1
 fi
-
-{{ if .Values.additionalSteps }}
-  {{- range .Values.additionalSteps }}
-  {{ . }}
-  {{- end }}
-{{- end }}
 
 echo "complete" > ${BACKUP_DIR}/${TIMESTAMP}.state
 
@@ -75,6 +65,6 @@ echo "Disk usage in ${BACKUP_DIR}"
 du -h -d 2 ${BACKUP_DIR}
 
 echo "Backup successful! :-)"
-{{ else }}
-echo "no mysql.host set in values file... nothing to do... exiting :)"
-{{ end }}
+else
+  echo "no mysql.host set in values file... nothing to do... exiting :)"
+fi
